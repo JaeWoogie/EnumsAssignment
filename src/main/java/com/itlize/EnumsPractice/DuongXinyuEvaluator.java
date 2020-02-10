a8ca64fc3a79171b6131367b7b5c288e2a97362d
JaeWoogie <60237088+JaeWoogie@users.noreply.github.com>
2020-02-09 22:13:39 -0500

    Add files via upload

---
+++ src/main/java/com/itlize/EnumsPractice/DuongXinyuEvaluator.java
@@ -0,0 +1,15 @@
+package com.itlize.EnumsPractice;
+
+public class DuongXinyuEvaluator {
+    String input;
+    public DuongXinyuEvaluator(String input) {
+        this.input = input;
+    }
+
+    public Boolean isDuong(){
+        return input.equalsIgnoreCase("Duong");
+    }
+
+    public Boolean isXinyu(){ return input.equalsIgnoreCase("Xinyu"); }
+
+}
